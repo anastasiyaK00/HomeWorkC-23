afd1fbdc8ae277f17c2ab0daa28b6d1680759d7c
Anastasiya <anastasiyak96.96@mail.ru>
2023-01-28 13:19:13 +0300

    1-4

---
+++ T4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.WriteLine("Введите число  ");
+int A = int.Parse(Console.ReadLine());
+
+if (A % 2 == 0)
+{
+    Console.WriteLine($"{A}-число четное");
+}
+else
+{
+    Console.WriteLine($"{A}-число Нечетное");
+}
