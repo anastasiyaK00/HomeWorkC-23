498dbadef164b235fc874d8ad71c9903b1411d18
Anastasiya <anastasiyak96.96@mail.ru>
2023-01-28 13:43:34 +0300

    5

---
+++ T5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать четные числа от 1 до N
+
+
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+//int k = 1;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i}");
+        i=i+1;
+    }
+    else
+i++;
+}
