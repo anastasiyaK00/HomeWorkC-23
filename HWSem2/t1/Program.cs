b306bda2b493f9fad8b09c288517f152a5a48bd0
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-03 22:23:47 +0300

    s

---
+++ HWSem2/t1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+/*
+int Monday = 1;
+int Tuesday = 2;
+int Wednesday = 3;
+int Thursday = 4;
+int Friday = 5; */
+int Saturday = 6;
+int Sunday = 7;
+
+Console.WriteLine("Введите номер дня недели ");
+int A = int.Parse(Console.ReadLine());
+ 
+ if (A == Saturday || A == Sunday)
+Console.WriteLine("Да, заданное вами число является выходным днем");
+else 
+Console.WriteLine("Нет, заданное число обозначает будний день");
