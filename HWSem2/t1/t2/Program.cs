be3113b370f435174650aad6c9fc27862e1152f1
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-04 10:24:35 +0300

    sem2

---
+++ HWSem2/t1/t2/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введите первое число:____");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число:____");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+if (A / B == B)
+    Console.WriteLine($"A = {A} - квадрат числа B = {B}");
+else if (B / A == A)
+    Console.WriteLine($"B = {B} - квадрат числа A = {A}");
+else
+    System.Console.WriteLine("Ни A ни B не являются квадратами");
