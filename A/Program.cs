8acb57eccd079e50b519dc145fe4cab02d67fbf0
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-05 23:16:21 +0300

    HW ce3

--- A/Program.cs
+++ A/Program.cs
@@ -2,16 +2,44 @@
 
 Console.WriteLine("Введите число: ");
 int S = int.Parse(Console.ReadLine() ?? "0");
-int k=S;
+int k = S;
 
 int i = 0;
 while (S > 0)
 {
     S = S / 10;
     i++;
-
 }
 Console.WriteLine(i);
+int[] array = new int[i - 1];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        k = k / 10;
+        collection[index] = k % 10;
+        index++;
+    }
+}
+void PrintArray(int[] col) // void-это метод, кот. ничего не возвращает 
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+FillArray(array); // Заполнил массив
+PrintArray(array); //Распечатал массив
+Console.WriteLine();
+
+
 
 /*
 int[] array = new int[i];
@@ -40,7 +68,6 @@ void PrintArray(int[] col)
 }
 FillArray(array);
 PrintArray(array);
-
 */
 /*
 int L(int i)
