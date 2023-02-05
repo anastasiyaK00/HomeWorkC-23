2b4e347f4fba1afccb0c33d18e7ff1305c28c7fc
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-05 16:30:19 +0300

    HW cem3

---
+++ HWSem3/A1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int i = 1;
+int k = 1;
+while (i < N + 1)
+{
+    k = i * i * i;
+    Console.WriteLine($"{i} в кубе = {k}");
+    i++;
+}
---
+++ HWSem3/A2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число: ");
+int S = int.Parse(Console.ReadLine() ?? "0");
+int k=S;
+
+int i=0;
+while (S!=0)
+{
+    S=S/10;
+    i++;
+}
+Console.WriteLine(i);
+
+
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = (1,S);
+        index++;
+    }
+}
