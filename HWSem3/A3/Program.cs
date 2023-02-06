1295e04c164bac920161e7ce61e3ca88e113e7be
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-06 21:24:31 +0300

    HW s3 A3

---
+++ HWSem3/A3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+
+int i = 1;
+int pr = 1;
+while (i < n + 1)
+{
+    pr = pr*i;
+    Console.WriteLine($"Произведение чисел от 1 до {i} = {pr}");
+    i++;
+}
+Console.WriteLine($"Ответ: {pr}");
