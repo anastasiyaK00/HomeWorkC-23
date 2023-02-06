73c0c05e6f962957c39a1f5553a7a09dcc542573
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-06 21:11:12 +0300

    HW s3 A2

--- HWSem3/A2/Program.cs
+++ HWSem3/A2/Program.cs
@@ -2,14 +2,13 @@
 
 Console.WriteLine("Введите число: ");
 int S = int.Parse(Console.ReadLine() ?? "0");
-//int k = S;
+
 int sum = 0;
-int i = 0;
+
 while (S > 0)
 {
     sum = sum + S % 10;
-    i++;
+    S = S / 10;
 }
-S = S / 10;
 
 Console.WriteLine(sum);
