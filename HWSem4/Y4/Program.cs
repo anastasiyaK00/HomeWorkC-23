6a42ec56336250f46f9bfc1e8379b85e430dfc45
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-09 21:11:21 +0300

    HW 4

--- HWSem4/Y4/Program.cs
+++ HWSem4/Y4/Program.cs
@@ -1,6 +1,45 @@
-﻿/* 4.Найти сумму чисел одномерного массива стоящих на нечетной позиции
-5.Найти произведение пар чисел в одномерном массиве. 
-Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-6.В Указанном массиве вещественных чисел 
-найдите разницу между максимальным и минимальным элементом
-*/
+﻿//4.Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int[] array = new int[5];
+
+void Fill(int[] a)
+{
+    int lenght = a.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        a[index] = new Random().Next(1, 199);
+        index++;
+    }
+}
+
+void Print(int[] b)
+{
+    int l = b.Length;
+    int p = 0;
+    while (l > p)
+    {
+        Console.WriteLine(b[p]);
+        p++;
+    }
+}
+
+void Sum(int[] s)
+{
+    int l = s.Length;
+    int sum = 0;
+    int i = 0;
+    while (i < l)
+    {
+        if (s[i] % 2 != 0)
+        {
+            sum = sum + s[i];
+        }
+          i++;
+    }
+    Console.WriteLine(sum);
+}
+    Fill(array);
+    Print(array);
+    Console.WriteLine("Сумма чисел стоящих на нечетной позиции: ");
+    Sum(array);
