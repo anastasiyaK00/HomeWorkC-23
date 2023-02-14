a4649edbeefa41e19a60f6525a42bf047031f8b9
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-14 22:13:07 +0300

    hwsem5.3

--- hwsem5/h2/Program.cs
+++ hwsem5/h2/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу масштабирования фигуры
+
+// Определим что за фигура перед нами ( сколько имеет сторон)
+System.Console.WriteLine("Введите количество сторон фигуры");
+int side = int.Parse(Console.ReadLine() ?? "0");
+
+//Зададим каждой стороне размер  при помощи массива
+
+int[] arr = new int [side];
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = int.Parse(Console.ReadLine() ?? "0");
+        index++;
+    }
+}
+void PrintArray(int[] array) //Метод, печатающий наш массив
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.Write(" ");
+}
+
+void mashtab (int[] mash)
+{
+    
+}
+
+
+
+System.Console.WriteLine("Введите длину сторон: ");
+FillArray(arr);
+System.Console.WriteLine("Заданная фигура имеет следующую длину сторон: ");
+PrintArray(arr);
---
+++ hwsem5/h3/Program.cs
@@ -0,0 +1,42 @@
+﻿// копирование массива
+
+
+System.Console.WriteLine("Введите количество элементов массива'");
+int s = int.Parse(Console.ReadLine() ?? "0");
+int [] array = new int [s];
+System.Console.WriteLine("Поочередено введите каждый элемент массива");
+
+FillArray(array);
+PrintArray(array);
+CopyArray(array);
+
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = int.Parse(Console.ReadLine() ?? "0");
+        index++;
+    }
+}
+void PrintArray(int[] array) 
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.Write(" ");
+}
+void CopyArray(int[] collection)
+{
+    int Length = array.Length;
+    int [] copy = new int [array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        copy[i]=array[i];
+    }
+    Console.Write(copy); Console.Write(",");
+}
