bb82eaa161982f0f0f5e72a367e1043aea7cc8f4
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-15 21:43:30 +0300

    hwsem5.2

--- hwsem5/h2/Program.cs
+++ hwsem5/h2/Program.cs
@@ -1,40 +1,68 @@
-﻿//Написать программу масштабирования фигуры
+﻿              //Написать программу масштабирования фигуры
 
-// Определим что за фигура перед нами ( сколько имеет сторон)
-System.Console.WriteLine("Введите количество сторон фигуры");
-int side = int.Parse(Console.ReadLine() ?? "0");
+//я не поняла про какую фигуру идет речь.Здесь можно было бы задать
+// количество сторон
+//ввести массив и таким образом получить любую фигуру, 
+//но я пока не понимаю 
+//как это сделать, поэтому возьму за основу 4угольник
 
-//Зададим каждой стороне размер  при помощи массива
+System.Console.WriteLine("Введите x1:");
+int x1 = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите y1:");
+int y1 = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите x2:");
+int x2 = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите y2:");
+int y2 = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите x3:");
+int x3 = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите y3:");
+int y3 = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите x4:");
+int x4 = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine("Введите y4:");
+int y4 = int.Parse(Console.ReadLine() ?? "0");
 
-int[] arr = new int [side];
-void FillArray(int[] collection)
+Console.WriteLine("Введите + если нужно увеличить или - чтобы уменьшить");
+string metod = Console.ReadLine();
+Console.WriteLine("Введите коэф. масштабирования");
+int k = int.Parse(Console.ReadLine() ?? "0");
+
+int[] Array = { x1, y1, x2, y2, x3, y3, x4, y4 };
+
+if (metod.ToLower() == "+")
 {
-    int Length = collection.Length;
-    int index = 0;
-    while (index < Length)
+    int length = Array.Length;
+    for (int i = 0; i < length; i++)
     {
-        collection[index] = int.Parse(Console.ReadLine() ?? "0");
-        index++;
+        Array[i] = Array[i] * k;
+    }
+
+}
+else if (metod.ToLower() == "-")
+{
+
+    int length = Array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Array[i] = Array[i] / k;
+    }
+
+}
+
+void PrintArray(int[] col) // void-это метод, кот. ничего не возвращает 
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+         Console.Write(col[position]);
+         Console.WriteLine(" ");
+        position++;
     }
 }
-void PrintArray(int[] array) //Метод, печатающий наш массив
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]}  ");
-    }
-    Console.Write(" ");
-}
-
-void mashtab (int[] mash)
-{
-    
-}
+PrintArray(Array);
 
 
-
-System.Console.WriteLine("Введите длину сторон: ");
-FillArray(arr);
-System.Console.WriteLine("Заданная фигура имеет следующую длину сторон: ");
-PrintArray(arr);
+// Я пsталась, постараюсь еще пересмотреть лекции и выполнить заново,
+//  но пока больше ничего придумать не могу..
