d19e8e7f5a73b08ce697ab59324e302fe4b2afa4
Anastasiya <anastasiyak96.96@mail.ru>
2023-03-01 21:50:52 +0300

    hsem7

---
+++ Sem7/a2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Написать программу, упорядочивания по убыванию
+//элементы каждой строки двумерной массива.
+int[,] matrix = new int[4, 5];
+
+
+void PrintArray(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+       for (int k = 0; k < matrix.GetLength(1); k++)
+       {
+           System.Console.Write($"{matrix[i, k]}");
+       }
+       System.Console.WriteLine();
+   }
+}
+void FillArray(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+       for (int k = 0; k < matrix.GetLength(1); k++)
+       {
+           matrix[i, k] = new Random().Next(1, 10);
+       }
+       System.Console.WriteLine();
+   }
+}
+
+
+FillArray(matrix);
+PrintArray(matrix);
+sort(matrix);
+System.Console.WriteLine("массив отсортированный: ");
+PrintArray(matrix);
+void sort(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+ {
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+     for (int k = 0; k < array.GetLength(1) - 1; k++)
+     {
+       if (array[i, k] < array[i, k + 1])
+       {
+         int temp = array[i, k + 1];
+         array[i, k + 1] = array[i, k];
+         array[i, k] = temp;
+       }
+     }
+   }
+ }
+}
+
+
---
+++ Sem7/a3/Program.cs
@@ -0,0 +1,65 @@
+﻿// В прямоугольной матрице найти строку
+//с наименьшей суммой элементов.
+
+
+int[,] matrix = new int[5, 2];
+
+
+void PrintArray(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+       for (int k = 0; k < matrix.GetLength(1); k++)
+       {
+           System.Console.Write($"{matrix[i, k]}");
+       }
+       System.Console.WriteLine();
+   }
+}
+void FillArray(int[,] matrix)
+{
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+       for (int k = 0; k < matrix.GetLength(1); k++)
+       {
+           matrix[i, k] = new Random().Next(1, 10);
+       }
+       System.Console.WriteLine();
+   }
+}
+
+
+
+
+FillArray(matrix);
+PrintArray(matrix);
+Sam(matrix);
+
+
+void Sam(int[,] array)
+{
+   int sum;
+   int max = 0;
+   int d;
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+       sum = 0;
+
+
+       System.Console.WriteLine(" ");
+       for (int k = 0; k < array.GetLength(1); k++)
+       {
+           sum = array[i, k] + sum;
+
+
+           if (sum > max)
+           {
+               max = sum;
+               d = i;
+           }
+       }
+  
+   System.Console.WriteLine($"сумма столбца {i} = {max}");
+}}
+
+
